689e790eecba5ba791f819e6341d64bed8c2f318
ryokf <ryokhrisnaf@gmail.com>
2025-06-03 17:28:17 +0700

    feat: add routes for sepia and edge detection pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,8 @@ import BlurPages from './pages/BlurPages.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import GrayscalePages from './pages/GrayscalePages.jsx'
 import HomePage from './pages/HomePage.jsx'
-
+import SepiaPages from './pages/SepiaPage.jsx'
+import EdgeDetectionPages from './pages/EdgeDetectionPages.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
@@ -13,6 +14,8 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<HomePage />} />
         <Route path="/blur" element={<BlurPages />} />
         <Route path="/grayscale" element={<GrayscalePages />} />
+        <Route path="/sepia" element={<SepiaPages />} />
+        <Route path="/edge-detection" element={<EdgeDetectionPages />} />
         <Route path="*" element={<div>404 Not Found</div>} />
       </Routes>
     </BrowserRouter>
